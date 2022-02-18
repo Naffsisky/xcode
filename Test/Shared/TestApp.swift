8a4f94c9f118028da9687f08169418b3a7b3db18
Nap <70518391+Naffsisky@users.noreply.github.com>
2022-02-18 18:48:55 +0700

    Add files via upload

---
+++ Test/Shared/TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestApp.swift
+//  Shared
+//
+//  Created by Prinaf Sika on 15/02/22.
+//
+
+import SwiftUI
+
+@main
+struct TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
